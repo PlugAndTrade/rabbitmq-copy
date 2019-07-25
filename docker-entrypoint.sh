d5b7290ee19ded1fecf6fad89e8935cdc7102e86
Henrik Jönsson <henke@plug-n-trade.com>
2019-07-25 13:27:44 +0200

    Default username and password

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rabbitmqadmin -H $RABBIT_SOURCE_HOST -u $RABBIT_SOURCE_USER -p $RABBIT_SOURCE_PASSWORD export /tmp/rabbit_config.json
-rabbitmqadmin -H $RABBIT_TARGET_HOST -u $RABBIT_TARGET_USER -p $RABBIT_TARGET_PASSWORD import /tmp/rabbit_config.json
+rabbitmqadmin -H $RABBIT_SOURCE_HOST -u ${RABBIT_SOURCE_USER:-guest} -p ${RABBIT_SOURCE_PASSWORD:-guest} export /tmp/rabbit_config.json
+rabbitmqadmin -H $RABBIT_TARGET_HOST -u ${RABBIT_TARGET_USER:-guest} -p ${RABBIT_TARGET_PASSWORD:-guest} import /tmp/rabbit_config.json
